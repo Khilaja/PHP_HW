8ff1c7d53c6f1808bd5bcc5acf0d54cb5d292847
Khilaja <1hvostaja@gmail.com>
2023-03-22 19:57:34 +0300

    finish homework

--- index.php
+++ index.php
@@ -1,5 +1,39 @@
 <?php
+$c = 5;
+echo $c + 3;
 
 
+$a = 10;
+$b = 7;
+echo $a / $b;
+
+$name = 'Алиса';
+$age = 25;
+echo "Меня зовут {$name} и мне {$age} лет";
+
+$f = 12;
+echo $f * 2;
+
+$x = 6;
+$y = 4;
+echo $x * $y;
+
+$m = 7;
+$n = 3;
+echo $m + $n;
+
+$exampleText = 'Я Таня и';
+echo $exampleText . ' мне нравится программирование';
+
+$g = 8;
+echo ++ $g;
+
+$firstName = 'Таня';
+$secondName = 'Иваненко';
+echo $firstName . ' ' . $secondName;
+
+$q = 12;
+$r = '12';
+echo $q + $r;
 
 ?>
